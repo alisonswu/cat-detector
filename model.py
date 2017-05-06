d1f522873f3c23d5a18fbde246539dba415dc248
Xiang Zhang <xzhang23@ncsu.edu>
2017-05-06 15:16:45 -0700

    remove fc layer

--- model.py
+++ model.py
@@ -8,9 +8,8 @@ class Net(nn.Module):
         self.pretrained_model = pretrained_model
         self.conv1= nn.Conv2d(512, 1024, kernel_size=3, stride=2, padding=1) 
         self.bn1 = nn.BatchNorm2d(1024)
-        self.relu = nn.ReLU(inplace=True)
-        self.fc1 = nn.Linear(7*7*1024, 1024)
-        self.fc2 = nn.Linear(1024, 7*7*5)
+        self.relu = nn.ReLU()
+        self.conv2= nn.Conv2d(1024, 5, kernel_size=5, stride=1, padding=2, bias=False)
         self.softsign = nn.Softsign()
         
     def forward(self, x):
@@ -18,10 +17,8 @@ class Net(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = x.view(-1,7,7,5)
+        x = self.conv2(x)
+        x = x.permute(0,2,3,1)
         x = (self.softsign(x) + 1)*0.5
         return x
 
@@ -31,10 +28,12 @@ class Net(nn.Module):
 # loss function
 # output and target must be variables
 #---------------------------------------------------- 
-def YOLOCriterion(output, target, lambda_coord=5, lambda_noobj=0.05):
-    loss_x = lambda_coord * torch.mul( target[:,:,:,4], (target[:,:,:,0] - output[:,:,:,0]).pow(2)).sum()
-    loss_y = lambda_coord * torch.mul( target[:,:,:,4], (target[:,:,:,1] - output[:,:,:,1]).pow(2)).sum()
+def YOLOCriterion(output, target, lambda_coord=5, lambda_obj=1, lambda_noobj=0.5):
+    loss_x = torch.mul( target[:,:,:,4], (target[:,:,:,0] - output[:,:,:,0]).pow(2)).sum()
+    loss_y = torch.mul( target[:,:,:,4], (target[:,:,:,1] - output[:,:,:,1]).pow(2)).sum()
     loss_h = lambda_coord * torch.mul( target[:,:,:,4], (target[:,:,:,2].pow(0.5) - output[:,:,:,2].pow(0.5)).pow(2)).sum()
     loss_w = lambda_coord * torch.mul( target[:,:,:,4], (target[:,:,:,3].pow(0.5) - output[:,:,:,3].pow(0.5)).pow(2)).sum()
-    loss_c = ((lambda_noobj + target[:,:,:,4]*(1-lambda_noobj))*(target[:,:,:,4]- output[:,:,:,4]).pow(2)).sum()
-    return loss_x, loss_y, loss_h, loss_w, loss_c 
+    loss_c = (target[:,:,:,4]- output[:,:,:,4]).pow(2)
+    loss_c_obj = lambda_obj*(target[:,:,:,4]*loss_c).sum()
+    loss_c_noobj = lambda_noobj*((1-target[:,:,:,4])*loss_c).sum()
+    return loss_x, loss_y, loss_h, loss_w, loss_c_obj, loss_c_noobj
